919101fb7d57a104b14bc07c1af32312163bf37c
Sibananda <sibanandadora443@gmail.com>
2026-03-28 23:32:03 +0530

    this or use .env and use os to import to .py files

---
+++ config.py
@@ -0,0 +1,8 @@
+EMBEDDING_MODEL = "nomic-embed-text:latest"
+ROUTER_MODEL = "llama3.2:3b"
+REASONING_MODEL = "deepseek-r1:7b"
+CHROMA_PATH = "./chroma_db"
+DATA_PATH = "./data"
+OLLAMA_BASE_URL = "http://localhost:11434" 
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
